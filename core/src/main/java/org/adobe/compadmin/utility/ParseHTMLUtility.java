f85ee8af1431e86805e8572ae9e8a569cf5d0806
Shivika Sharma <shiviksh@adobe.com>
2018-03-23 15:10:38 +0530

    adding fileReference change.

--- core/src/main/java/org/adobe/compadmin/utility/ParseHTMLUtility.java
+++ core/src/main/java/org/adobe/compadmin/utility/ParseHTMLUtility.java
@@ -22,119 +22,143 @@ import org.jsoup.select.NodeVisitor;
 import com.google.gson.Gson;
 
 public class ParseHTMLUtility {
+	
+	private static final String HTML_TAG_PATTERN = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>";
+	private static Pattern pattern;
+	private static Matcher matcher;
+	private static Map<String, String> ruleMap = new LinkedHashMap<String, String>();
+	private static Map<String, String> myMap = new LinkedHashMap<String, String>();
+	
+	public static String parseHTMLUsingRuleMap(String html) {
+		ruleMap.put("a", "attribute,href,pathfield");
+		ruleMap.put("img", "attribute,src,image");
+		ruleMap.put("button", "arrtribute,href,pathfield");
 
-       private static final String HTML_TAG_PATTERN = "<(\"[^\"]*\"|'[^']*'|[^'\">])*>";
-       private static Pattern pattern;
-       private static Matcher matcher;
-       private static Map<String, String> ruleMap = new LinkedHashMap<String, String>();
-       private static Map<String, String> myMap = new LinkedHashMap<String, String>();
+		Document document = Jsoup.parseBodyFragment(html);
+		document.traverse(new NodeVisitor() {
+			int counter = 0;
 
-       public static String parseHTMLUsingRuleMap(String html) {
-    	     ruleMap.put("a", "attribute,href,pathfield");
-             ruleMap.put("img", "attribute,src,image");
-             ruleMap.put("button", "arrtribute,href,pathfield");
-           
-             Document document = Jsoup.parseBodyFragment(html);
-             document.traverse(new NodeVisitor() {
-                    int counter = 0;
-                    public void head(Node node, int depth) {
-                          // TextField
-                          if ((node instanceof TextNode && !((TextNode) node).isBlank())) {
-                                 String replacementValue = "${properties.element"
-                                              + (counter) + "}";
-                                 String xtype = validateTextType(((TextNode) node).text());
-                                 myMap.put(replacementValue, xtype);
-                                 // replace val
-                                 Node myNode = node.parent();
-                                 Element element = (Element) myNode;
-                                 element.text(replacementValue);
-                                 
-                                 counter++;
+			public void head(Node node, int depth) {
+				// TextField
+				if ((node instanceof TextNode && !((TextNode) node).isBlank())) {
+					String replacementValue = "${properties.element"
+							+ (counter) + "}";
+					String xtype = validateTextType(((TextNode) node).text());
+					myMap.put(replacementValue, xtype);
+					// replace val
+					Node myNode = node.parent();
+					Element element = (Element) myNode;
+					if(null != element){
+					 element.text(replacementValue);
+					}
 
-                          } else if (node.nodeName().equalsIgnoreCase("img") || node.nodeName().equalsIgnoreCase("a") || node.nodeName().equalsIgnoreCase("button")) {
-                                 
-                                 String replacementValue = "${properties.element"
-                                              + (counter) + "}";
-                                 
-                                 if(ruleMap.containsKey(node.nodeName())){
-                                       String val = ruleMap.get(node.nodeName());
-                                       String[] data = val.split(",");
-                                        if(data[0].equalsIgnoreCase("attribute")){
-                                              node.attr(data[1], replacementValue);
-                                              myMap.put(replacementValue, data[2]);
+					counter++;
 
-                                       }
-                                       
-                                 }
-                                 counter++;
+				} else if (node.nodeName().equalsIgnoreCase("a")
+						|| node.nodeName().equalsIgnoreCase("button")) {
 
-                          }
-                          
-                    }
+					String replacementValue = "${properties.element"
+							+ (counter) + "}";
 
-                    public void tail(Node node, int depth) {
+					if (ruleMap.containsKey(node.nodeName())) {
+						String val = ruleMap.get(node.nodeName());
+						String[] data = val.split(",");
+						if (data[0].equalsIgnoreCase("attribute")) {
+							node.attr(data[1], replacementValue);
+							myMap.put(replacementValue, data[2]);
 
-                    }
-             });
-             Gson gson = new Gson();
-        String dialogString = gson.toJson(myMap);
-        JSONObject finalResponse = new JSONObject();
-        try {
-            JSONObject dialog = new JSONObject(dialogString);
+						}
 
-            finalResponse.put("dialog", dialog);
-            finalResponse.put("processedHtml", document.body().html());
+					}
+					counter++;
 
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        return finalResponse.toString();
-           
+				}else if (node.nodeName().equalsIgnoreCase("img")) {
 
-       }
+					String replacementValue = "${properties.fileReference"
+							+ (counter) + "}";
 
-       private static String validateTextType(String text) {
-             pattern = Pattern.compile(HTML_TAG_PATTERN);
-             matcher = pattern.matcher(text);
-             if (matcher.matches()) {
-                    return "richtext";
-             } else {
-                    return "textfield";
-             }
-       }
+					if (ruleMap.containsKey(node.nodeName())) {
+						String val = ruleMap.get(node.nodeName());
+						String[] data = val.split(",");
+						if (data[0].equalsIgnoreCase("attribute")) {
+							node.attr(data[1], replacementValue);
+							myMap.put(replacementValue, data[2]);
 
-    public static String getBody(SlingHttpServletRequest request) throws IOException {
+						}
 
-        String body = null;
-        StringBuilder stringBuilder = new StringBuilder();
-        BufferedReader bufferedReader = null;
+					}
+					counter++;
 
-        try {
-            InputStream inputStream = request.getInputStream();
-            if (inputStream != null) {
-                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-                char[] charBuffer = new char[128];
-                int bytesRead = -1;
-                while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
-                    stringBuilder.append(charBuffer, 0, bytesRead);
-                }
-            } else {
-                stringBuilder.append("");
-            }
-        } catch (IOException ex) {
-            throw ex;
-        } finally {
-            if (bufferedReader != null) {
-                try {
-                    bufferedReader.close();
-                } catch (IOException ex) {
-                    throw ex;
-                }
-            }
-        }
+				}
 
-        body = stringBuilder.toString(); //send body.
-        return body;
-    }
+			}
+
+			public void tail(Node node, int depth) {
+
+			}
+		});
+		Gson gson = new Gson();
+		String dialogString = gson.toJson(myMap);
+		JSONObject finalResponse = new JSONObject();
+		try {
+			JSONObject dialog = new JSONObject(dialogString);
+			String placeholderHTML ="<div data-sly-test=\"${wcmmode.edit &amp;&amp; !properties.element0}\" class=\"cq-placeholder\"\n" +
+               "data-emptytext=\"Please configure the component here..\" ></div>";
+			Element firstDiv = document.select("div").first();
+			firstDiv.before(placeholderHTML);
+			finalResponse.put("dialog", dialog);
+			finalResponse.put("processedHtml", document.body().html());
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return finalResponse.toString();
+
+	}
+
+	private static String validateTextType(String text) {
+		pattern = Pattern.compile(HTML_TAG_PATTERN);
+		matcher = pattern.matcher(text);
+		if (matcher.matches()) {
+			return "richtext";
+		} else {
+			return "textfield";
+		}
+	}
+
+	public static String getBody(SlingHttpServletRequest request)
+			throws IOException {
+
+		String body = null;
+		StringBuilder stringBuilder = new StringBuilder();
+		BufferedReader bufferedReader = null;
+
+		try {
+			InputStream inputStream = request.getInputStream();
+			if (inputStream != null) {
+				bufferedReader = new BufferedReader(new InputStreamReader(
+						inputStream));
+				char[] charBuffer = new char[128];
+				int bytesRead = -1;
+				while ((bytesRead = bufferedReader.read(charBuffer)) > 0) {
+					stringBuilder.append(charBuffer, 0, bytesRead);
+				}
+			} else {
+				stringBuilder.append("");
+			}
+		} catch (IOException ex) {
+			throw ex;
+		} finally {
+			if (bufferedReader != null) {
+				try {
+					bufferedReader.close();
+				} catch (IOException ex) {
+					throw ex;
+				}
+			}
+		}
+
+		body = stringBuilder.toString(); // send body.
+		return body;
+	}
 }
